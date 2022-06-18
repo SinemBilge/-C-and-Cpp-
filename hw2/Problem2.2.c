8a81723f881a3e6458fcf864a99e4c81632ddee8
SinemBilge <106627414+SinemBilge@users.noreply.github.com>
2022-06-18 03:02:24 +0300

    Delete hw2 directory

--- hw2/Problem2.2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    char c;
-    scanf("%c\n",&c);
-    printf("character=%c\n",c);
-    printf("decimal=%d\n",c);
-    printf("octal=%o\n",c);
-    printf("hexadecimal=%x\n",c);
-}
